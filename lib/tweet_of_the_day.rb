3ba715b50278cd3c53f63b725276c0b1af200eac
mjago <martyn.jago@btinternet.com>
2016-11-18 08:56:12 +0000

    Add local dependencies

--- lib/tweet_of_the_day.rb
+++ lib/tweet_of_the_day.rb
@@ -1,3 +1,10 @@
 require_relative 'totd/totd'
 require_relative 'totd/read_feed'
+require_relative 'totd/tic'
+require_relative 'totd/keyboard_events'
+require_relative 'totd/play_time'
+require_relative 'totd/player'
+#require_relative 'mpg123_player'
+#require_relative 'afplay_player'
+
 TweetOfTheDay.new
